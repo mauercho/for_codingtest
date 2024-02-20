7817783038892fa4e01e20484618fc1216b48508
minwucho <mauercho@gmail.com>
2024-02-20 23:50:10 +0900

    240220

---
+++ silver/14675.py
@@ -0,0 +1,4 @@
+N = int(input())
+cut_line_lst = []
+for i in range(1, N):
+    cut_line_lst.append()
--- silver/2628.py
+++ silver/2628.py
@@ -3,16 +3,22 @@ row, col = map(int, input().split())
 
 N = int(input())
 ans = -math.inf
-
+lst_row = [0]
+lst_col = [0]
 for _ in range(N):
     a, b = map(int, input().split())
-    lst_row = []
-    lst_col = []
     if a == 0:
-        lst_col.append(b)     
+        lst_col.append(b)
     else:
-        lst_col.append(a)
-    lst_row
-for i in sorted(lst_col):
-    for j in sorted(lst_col):
-        
+        lst_row.append(b)
+lst_row.append(row)
+lst_col.append(col)
+lst_col.sort()
+lst_row.sort()
+
+for i in range(len(lst_col) - 1):
+    for j in range(len(lst_row) - 1):
+        area = (lst_row[j + 1] - lst_row[j]) * (lst_col[i + 1] - lst_col[i])
+        ans = max(ans, area)
+
+print(ans)
