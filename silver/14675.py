917e74ed99246f9fb0c5a4afe4d671862b99cfa7
mauercho <mauercho@gmail.com>
2024-02-21 13:54:13 +0900

    240221 not yet

--- silver/14675.py
+++ silver/14675.py
@@ -1,4 +1,21 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-cut_line_lst = []
+node_info = [[] for _ in range(N + 1)]
+
 for i in range(1, N):
-    cut_line_lst.append()
+    a, b = map(int, input().split())
+    node_info[a].append(b)
+    node_info[b].append(b)
+
+q = int(input())
+for _ in range(q):
+    t, k = map(int, input().split())
+    if t == 1:
+        if len(node_info[k]) < 2:
+            print("no")
+        else:
+            print("yes")
+    elif t == 2:
+        print("yes")
