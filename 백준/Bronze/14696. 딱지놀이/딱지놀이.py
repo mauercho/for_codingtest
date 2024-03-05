d9a1729bcbc203fdf3a70c89a17daf4ca4c6de27
mauercho <156387215+mauercho@users.noreply.github.com>
2024-03-05 12:53:10 +0900

    [Bronze I] Title: 딱지놀이, Time: 236 ms, Memory: 31252 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/14696. 딱지놀이/딱지놀이.py
@@ -0,0 +1,19 @@
+N = int(input())
+
+for _ in range(N):
+	A_lst = [0] * 5
+	B_lst = [0] * 5
+	who_win = 'D'
+	for mem in input().split()[1:]:
+		A_lst[int(mem)] += 1
+	for mem in input().split()[1:]:
+		B_lst[int(mem)] += 1
+	for i in range(4 , 0, -1):
+		if A_lst[i] > B_lst[i]:
+			who_win = 'A'
+			break
+		elif A_lst[i] < B_lst[i]:
+			who_win = 'B'
+			break
+	print(f"{who_win}")
+
