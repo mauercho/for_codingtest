9b6f574a779cc56f8e1cadc4faf8c477c9c2988d
mauercho <156387215+mauercho@users.noreply.github.com>
2024-03-16 23:33:43 +0900

    [Silver V] Title: 덩치, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+arr = []
+for _ in range(N):
+    x, y = map(int, input().split())
+    arr.append((x, y))
+
+for i in range(N):
+    cnt = 1
+    for j in range(N):
+        if i == j:
+            continue
+        if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1]:
+            cnt += 1
+    print(cnt, end =' ')
