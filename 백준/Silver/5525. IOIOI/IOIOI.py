0b06ca1aaf80775cc6abb68bfb4a158ea247fc0a
mauercho <156387215+mauercho@users.noreply.github.com>
2024-03-30 11:56:06 +0900

    [Silver I] Title: IOIOI, Time: 404 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,45 +2,21 @@ import sys
 
 input = sys.stdin.readline
 
-def check(i):
-    x = 0
-    while x < n:
-        if string[i + 1 + 2 * x] != 'O':
-            return False
-        if string[i + 2 + 2 * x] != 'I':
-            return False
-        x += 1
-    return True
-
-def check_2():
-    global i
-
-    if string[i] == 'O' and string[i + 1] == 'I':
-        i += 2
-        return True
-    else:
-        return False
-
-
-n = int(input())
+N = int(input())
 l = int(input())
-length = 2 * n + 1
 string = input().rstrip()
 
-i = 0
+ans = 0
 cnt = 0
-
-
-while i <= l - length:
-    if string[i] == 'I':
-        if check(i):
-            cnt += 1
-            i += length
-            while i + 1 < l and check_2():
-                cnt += 1
-        else:
-            i += 1
+i = 0
+while i < l - 2:
+    if string[i:i+3] == 'IOI':
+        i += 2
+        cnt += 1
+        if cnt == N:
+            ans += 1
+            cnt -= 1
     else:
-        i += 1
-
-print(cnt)
+        i+=1
+        cnt = 0
+print(ans)
