ca798163be1d6f3c7508a20ed751f03cd3b297a3
mauercho <156387215+mauercho@users.noreply.github.com>
2024-04-07 23:45:27 +0900

    [Gold V] Title: 리모컨, Time: 296 ms, Memory: 110724 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -1,37 +1,18 @@
 import sys
-from itertools import product
-
 input = sys.stdin.readline
+target = int(input())
+n = int(input())
+broken = list(map(int, input().split()))
 
-num = int(input())
-length = len(str(num))
+min_count = abs(100 - target)
 
+for nums in range(1000001):
+    nums = str(nums)
+    
+    for j in range(len(nums)):
+        if int(nums[j]) in broken:
+            break
+        elif j == len(nums) - 1:
+            min_count = min(min_count, abs(int(nums) - target) + len(nums))
 
-N = int(input())
-number = ['0','1','2','3','4','5','6','7','8','9']
-
-if N != 0:
-    for mem in input().rstrip().split():
-        number.remove(mem)
-
-
-num_lst = []
-for k in range(1, length + 2):
-    for mem in product(number, repeat = k):
-        num_lst.append(int((''.join(mem))))
-ans = float('inf')
-
-for mem in num_lst:
-    if num > mem:
-        compare = num - mem
-    else:
-        compare = mem - num
-    ans = min(ans, compare + len(str(mem)))
-
-if num > 100:
-    compare = num - 100
-else:
-    compare = 100 - num
-ans = min(ans, compare)
-
-print(ans)
+print(min_count)
