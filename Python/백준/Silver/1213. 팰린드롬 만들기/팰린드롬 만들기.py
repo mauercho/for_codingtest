276c6a308b91225b58bad3d6d6ccaa733e6a6441
mauercho <156387215+mauercho@users.noreply.github.com>
2024-08-08 08:57:08 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ Python/백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -0,0 +1,22 @@
+s = input()
+c = dict()
+for ch in s:
+    if ch in c:
+        c[ch] += 1
+    else:
+        c[ch] = 1
+
+if sum(i % 2 for i in c.values()) > 1:
+    print("I'm Sorry Hansoo")
+else:
+    half = ''
+    for k, v in sorted(c.items()):
+        half += k * (v//2)
+    ans = half
+    for k, v in c.items():
+        if v % 2:
+            ans += k
+            break
+        
+    ans += ''.join(reversed(half))
+    print(ans)
