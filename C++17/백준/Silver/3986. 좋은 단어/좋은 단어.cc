dc7119b503b96ba65a71b443e3e0994adcc95cf9
mauercho <156387215+mauercho@users.noreply.github.com>
2024-08-09 00:17:49 +0900

    [Silver IV] Title: 좋은 단어, Time: 4 ms, Memory: 2392 KB -BaekjoonHub

--- C++17/백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ C++17/백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -1,33 +1,19 @@
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
-
-int n;
-int main(){
-	int cnt = 0;
-	string temp;
+int n, ret;
+string s;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 	cin >> n;
-	for (int i = 0; i < n; i++){
-		cin >> temp;
-		vector<char> h;
- 		bool isTrue = 1;
-		for (char mem : temp){
-			if (h.size() == 0){
-				h.push_back(mem);
-			}
-			else {
-				if (h[h.size() - 1] == mem){
-					while (h[h.size() - 1] == mem)
-						h.pop_back();
-				}
-				else {
-					h.push_back(mem);
-				}
-			}
+	for(int i = 0; i < n; i++){
+		cin >> s;
+		stack<char> stk;
+		for(char a : s){
+			if(stk.size() && stk.top() == a)stk.pop();
+			else stk.push(a);
 		}
-		if (h.size() == 0) {
-			cnt += 1;
-		}
+		if(stk.size() == 0)ret++;
 	}
-	cout << cnt;
+	cout << ret << "\n";
 }
