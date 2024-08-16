e3e42c47094d02ee8e83a9e303c25a8766bd1346
mauercho <156387215+mauercho@users.noreply.github.com>
2024-08-16 14:47:22 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- C++17/백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ C++17/백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,33 +1,39 @@
 #include <bits/stdc++.h>
-using namespace std;  
-typedef long long ll;
-const ll INF = 1e18;   
-int n, c, a[1004];
-vector<pair<int, int>> v; 
-map<int, int> mp, mp_first; 
-bool cmp(pair<int,int> a, pair<int, int> b){
-	if(a.first == b.first){
-		return mp_first[a.second] < mp_first[b.second];
-	}
-	return a.first > b.first;
+
+using namespace std;
+
+int n, c;
+int tmp;
+map<int , int> mp, mp_first;
+vector<pair<int, int>> vc;
+
+bool cmp(pair<int, int> a, pair<int, int> b) {
+	if (a.second == b.second)
+		return mp_first[a.first] < mp_first[b.first] ;
+	return a.second > b.second;
 }
-int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> n >> c; 
-	for(int i = 0; i < n; i++){
-		cin >> a[i];mp[a[i]]++;
-		if(mp_first[a[i]] == 0) mp_first[a[i]] = i + 1; 
-	} 
-	for(auto it : mp){
-		v.push_back({it.second, it.first});
-	}
-	sort(v.begin(), v.end(), cmp);
-	for(auto i : v){
-		for(int j = 0; j < i.first; j++){
-			cout << i.second << " ";
+
+
+int main(void) {
+	cin >> n >> c;
+	for (int i = 0; i < n; i++) {
+		cin >> tmp;
+		if (!mp_first[tmp]) {
+			mp_first[tmp] = i + 1;
 		}
-	} 
-	 
-	return 0;
+		if (mp[tmp])
+			mp[tmp] += 1;
+		else
+			mp[tmp] = 1;
+	}
+	for (auto mem : mp){
+		vc.push_back({ mem.first, mem.second });
+	}
+
+	sort(vc.begin(), vc.end(), cmp);
+	for (auto i : vc) {
+		for (int j = 0; j < i.second; j++) {
+			cout << i.first << " ";
+		}
+	}
 }
