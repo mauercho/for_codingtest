00f44620e272837e9dfb0eb6a7f03ed62dca2a51
mauercho <156387215+mauercho@users.noreply.github.com>
2024-11-09 11:25:15 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 56 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ Python/백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,53 +1,43 @@
-from itertools import permutations
-
 n = int(input())
-ans_min =1000000001
-ans_max= -1000000001
-def cal(a, b, oper):
-    if oper == '+':
-        return a + b
-    elif oper == '-':
-        return a - b
-    elif oper == '*':
-        return a * b
+# 연산을 수행하고자 하는 수 리스트
+data = list(map(int, input().split()))
+# 더하기, 빼기, 곱하기, 나누기 연산자 개수
+add, sub, mul, div = map(int, input().split())
+
+# 최솟값과 최댓값 초기화
+min_value = float('inf')
+max_value = float('-inf')
+
+# DFS 메서드
+def dfs(i, now):
+    global min_value, max_value, add, sub, mul, div
+
+    # 모든 연산자를 다 사용한 경우, 최솟값과 최댓값 업데이트
+    if i == n:
+        min_value = min(min_value, now)
+        max_value = max(max_value, now)
     else:
-        if a < 0:
-            return (-1) * (a * (-1) // b)
-        else:
-            return a // b
+        # 각 연산자에 대하여 재귀적으로 수행
+        if add > 0:
+            add -= 1
+            dfs(i + 1, now + data[i])
+            add += 1
+        if sub > 0:
+            sub -= 1
+            dfs(i + 1, now - data[i])
+            sub += 1
+        if mul > 0:
+            mul -= 1
+            dfs(i + 1, now * data[i])
+            mul += 1
+        if div > 0:
+            div -= 1
+            dfs(i + 1, int(now / data[i]))  # 나눌 때는 나머지를 제거
+            div += 1
 
-arr = list(map(int, input().split()))
-plus, minus, mul, div = map(int, input().split())
-overlap = set()
+# DFS 메서드 호출
+dfs(1, data[0])
 
-lst = tuple()
-for _ in range(plus):
-    lst += ('+', )
-for _ in range(minus):
-	lst += ('-', )
-for _ in range(mul):
-	lst += ('*', )
-for _ in range(div):
-	lst += ('/', )
-
-for mem in permutations(lst, n - 1):
-    string = ''.join(mem)
-    if string in overlap:
-        continue
-    overlap.add(string)
-    num = arr[0]
-    flag = True
-    for j in range(n - 1):
-        num = cal(num, arr[j + 1], mem[j])
-        if num >= 1000000001:
-            flag = False
-            break
-        elif num <= -1000000001:
-            flag = False
-            break
-    if flag:
-        ans_max = max(ans_max, num)
-        ans_min = min(ans_min, num)
-
-print(ans_max)
-print(ans_min)
+# 최댓값과 최솟값 차례대로 출력
+print(max_value)
+print(min_value)
