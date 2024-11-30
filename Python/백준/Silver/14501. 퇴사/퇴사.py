73ce6b798fa701d45f3bb39663f865e5b2c00fde
minwucho <156387215+mauercho@users.noreply.github.com>
2024-11-30 14:40:00 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- Python/백준/Silver/14501. 퇴사/퇴사.py
+++ Python/백준/Silver/14501. 퇴사/퇴사.py
@@ -2,23 +2,17 @@ import sys
 
 input = sys.stdin.readline
 
-def dfs(depth, sum_val):
-    global ans
-    if depth >= n:
-        ans = max(ans, sum_val)
-        return
-    if t_arr[depth] + depth <= n:
-        dfs(depth + t_arr[depth], sum_val + p_arr[depth])
-    dfs(depth + 1, sum_val)
-
 n = int(input())
-ans = 0
 t_arr = [0] * n
 p_arr = [0] * n
+dp = [0] * (n + 1)
 
 for i in range(n):
     t_arr[i], p_arr[i] = map(int, input().split())
 
-dfs(0, 0)
-
-print(ans)
+for i in range(n - 1, -1, -1):
+    if t_arr[i] + i <= n:
+        dp[i] = max(dp[i + 1], dp[t_arr[i] + i] + p_arr[i])
+    else:
+        dp[i] = dp[i + 1]
+print(dp[0])
