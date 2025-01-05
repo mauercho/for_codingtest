326c24d7b635bf322c39aa048975814f2cf92922
minwucho <156387215+mauercho@users.noreply.github.com>
2025-01-05 16:27:40 +0900

    [level 3] Title: 기둥과 보 설치, Time: 91.12 ms, Memory: 10.5 MB -BaekjoonHub

--- Python3/프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
+++ Python3/프로그래머스/3/60061. 기둥과 보 설치/기둥과 보 설치.py
@@ -1,26 +1,32 @@
-def possible(arr):
-    for mem in arr:
+def check(s): # 안되면 false return 
+    for mem in s:
         x, y, a = mem
         if a == 0: # 기둥
-            if y == 0 or [x - 1, y, 1] in arr or [x, y, 1] in arr or [x, y - 1, 0] in arr:
-                continue
-            return False
-        else: # 보 
-            if [x, y - 1, 0] in arr or [x + 1, y - 1, 0] in arr or ([x - 1, y, 1] in arr and [x + 1, y, 1] in arr):
-                continue
-            return False
+            if (x, y - 1, 0) in s or y == 0 or (x - 1, y, 1) in s or (x, y, 1) in s:
+                pass
+            else:
+                return False
+        else: # 보   
+            if ((x - 1, y, 1) in s and (x + 1, y, 1) in s) or (x, y - 1, 0) in s or (x + 1, y - 1, 0) in s:
+                pass
+            else:
+                return False 
     return True
 
 def solution(n, build_frame):
-    arr = []
-    for frame in build_frame:
-        x, y, a, b = frame
-        if b == 1: # 설치 
-            arr.append([x, y, a])
-            if not possible(arr):
-                arr.pop()
-        else: # 삭제   
-            arr.remove([x, y, a])
-            if not possible(arr):
-                arr.append([x, y, a])
-    return sorted(arr)
+    answer = [[]] 
+    s = set()
+    for oper in build_frame:
+        if oper[3] == 1: # 설치
+            s.add((oper[0], oper[1], oper[2]))
+            if not check(s):
+                s.remove((oper[0], oper[1], oper[2]))
+        else: # 제거
+            if (oper[0], oper[1], oper[2]) in s:
+                s.remove((oper[0], oper[1], oper[2]))
+                if not check(s):
+                    s.add((oper[0], oper[1], oper[2]))
+                
+    ans = sorted(s)
+    answer = list(map(list, ans))
+    return answer
