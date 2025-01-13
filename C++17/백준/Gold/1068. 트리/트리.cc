cdd6f4f4a18892d8e53263eed9969ef69ef41bb9
minwucho <156387215+mauercho@users.noreply.github.com>
2025-01-13 15:38:38 +0900

    [Gold V] Title: 트리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- C++17/백준/Gold/1068. 트리/트리.cc
+++ C++17/백준/Gold/1068. 트리/트리.cc
@@ -1,31 +1,37 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
-int n, r, temp, root;
-vector<int> adj[54];
-int dfs(int here) {
-    int ret = 0;
-    int child = 0;
-    for (int there : adj[here]) {
-        if (there == r) continue;
-        ret += dfs(there);
-        child++;
-    }
-    if (child == 0) return 1;
-    return ret;
+int n, root, r;
+int input_num;
+vector<int> graph[54];
+
+int dfs(int start){
+	int ret = 0;
+	int child = 0;
+
+	for (int next : graph[start]){
+		if (next == r) continue;
+		ret += dfs(next);
+		child += 1;
+	}
+	if (child == 0) return 1;
+	return ret;
 }
-int main() {
-    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cin >> temp;
-        if (temp == -1)root = i;
-        else adj[temp].push_back(i);
-    }
-    cin >> r;
-    if (r == root) {
-        cout << 0 << "\n"; 
-        return 0;
-    }
-    cout << dfs(root) << "\n";
-    return 0;
+
+int main(){
+	cin >> n;
+	for (int i = 0; i < n; i++){
+		cin >> input_num;
+		if (input_num == -1) root = i;
+		graph[input_num].push_back(i);
+	}
+
+	cin >> r;
+	if (r == root){
+		cout << 0;
+		return 0;
+	}
+	cout << dfs(root);
+	return 0;
 }
