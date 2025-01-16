6550c58000f9f872eeaebf5249e941f3a9fb867b
minwucho <156387215+mauercho@users.noreply.github.com>
2025-01-16 11:21:04 +0900

    [Gold IV] Title: 다이어트, Time: 4 ms, Memory: 2208 KB -BaekjoonHub

--- C++17/백준/Gold/19942. 다이어트/다이어트.cc
+++ C++17/백준/Gold/19942. 다이어트/다이어트.cc
@@ -1,55 +1,54 @@
-#include <map>
 #include <iostream>
 #include <vector>
-#include <algorithm>
-using namespace std;
+#include <cmath>
+#include <string>
 
-int N;
-struct {
-	int p, f, s, v, c;
-} arr[16];
-map<int, vector<vector<int>>> ret_v;
-const int INF = 987654321;
+using namespace std;
+int n;
 int mp, mf, ms, mv;
-int b, c, d, e, sum;
-int ret = INF;
+int p[16], f[16], s[16], v[16], c[16];
+int temp_cost, t_p, t_f, t_s, t_v;
+const int INF = 987654321;
+int ans = INF;
+vector<int> vec;
+vector<int> ans_vec;
 int main(){
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	cin >> N;
+	cin >> n;
+	int ref = (int)pow(2, n);
 	cin >> mp >> mf >> ms >> mv;
-	for (int i = 0; i < N; i++){
-		cin >> arr[i].p >> arr[i].f >> arr[i].s >> arr[i].v >> arr[i].c;
+	for (int i = 0; i < n; i++){
+		cin >> p[i] >> f[i] >> s[i] >> v[i] >> c[i];
 	}
-	for (int i = 1; i < (1 << N); i++){
-		b = c = d = e = sum = 0;
-		vector<int> v;
-		for (int j = 0; j < N; j++){
-			if (i & (1 << j)){
-				v.push_back(j + 1);
-				b += arr[j].p;
-				c += arr[j].f;
-				d += arr[j].s;
-				e += arr[j].v;
-				sum += arr[j].c;
+	for (int num = 1; num < ref; num++){
+		temp_cost = t_p = t_f = t_s = t_v = 0;
+		vec.clear();
+		for (int pos = 0; pos < n; pos++){
+			if (num & (1 << pos)){
+				vec.push_back(pos + 1);
+				temp_cost += c[pos];
+				t_p += p[pos];
+				t_f += f[pos];
+				t_s += s[pos];
+				t_v += v[pos];
+			}
+			if (temp_cost > ans) break;
+		}
+		if (t_p >= mp && t_f >= mf && t_s >= ms && t_v >= mv){
+			if (temp_cost < ans){
+				ans = temp_cost;
+				ans_vec = vec;
+			}
+			else if (temp_cost == ans){
+				if (ans_vec > vec){
+					ans_vec = vec;
+				}
 			}
 		}
-		if (b >= mp && c >= mf && d >= ms && e >= mv){
-			if (ret >= sum){
-				ret = sum;
-				ret_v[ret].push_back(v);
-			}
-		}
 	}
-	if (ret == INF)
-		cout << -1;
+	if (ans == INF) cout << -1;
 	else {
-		cout << ret << "\n";
-		sort(ret_v[ret].begin(), ret_v[ret].end());
-		for (int a : ret_v[ret][0]){
-			cout << a << " ";
-		}
+		cout <<ans << "\n";
+		for (int i = 0; i < ans_vec.size(); i++) cout << ans_vec[i] << " ";
 	}
 	return 0;
 }
