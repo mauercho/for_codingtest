608e5a7e19a5846fa5388ea1f185ce60c4a7befc
minwucho <156387215+mauercho@users.noreply.github.com>
2025-03-01 20:28:30 +0900

    [level 2] Title: 할인 행사, Time: 62.28 ms, Memory: 43.1 MB -BaekjoonHub

--- JavaScript/프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ JavaScript/프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,35 +1,15 @@
 function solution(want, number, discount) {
     var answer = 0;
     obj = {};
-    for (let idx = 0; idx < number.length; idx++){
+    for (let idx = 0; idx < want.length; idx++){
         obj[want[idx]] = number[idx];
     }
-    for (let idx = 0; idx < 10; idx++){
-        if (obj[discount[idx]] !== undefined){
-            obj[discount[idx]] -= 1;   
+    for (let cnt = 0; cnt <= discount.length - 10; cnt++){
+        const tempObj = {...obj};
+        for (let i = cnt; i < cnt + 10; i++){
+            if (tempObj[discount[i]] !== undefined) tempObj[discount[i]]--;
         }
-    }
-    answer += 1;
-    for (const [key, val] of Object.entries(obj)){
-        if (val !== 0){
-            answer -= 1;
-            break;
-        } 
-    }
-    for (let cnt = 1; cnt <= discount.length - 10; cnt++){
-        if (obj[discount[cnt + 9]] !== undefined) {
-            obj[discount[cnt + 9]] -= 1;
-        } 
-        if (obj[discount[cnt - 1]] !== undefined){
-            obj[discount[cnt - 1]] += 1;
-        }
-        answer += 1;
-        for (const [key, val] of Object.entries(obj)){
-            if (val !== 0){
-                answer -= 1;
-                break;
-            } 
-        }
+        if (Object.values(tempObj).every((v) => v === 0)) answer += 1;
     }
     return answer;
 }
