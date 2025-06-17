c6aeb947d278ac9f5b8d1234080f009180c5e001
minwucho <156387215+mauercho@users.noreply.github.com>
2025-06-17 22:41:37 +0900

    [Gold IV] Title: 자두나무, Time: 0 ms, Memory: 2272 KB -BaekjoonHub

--- C++17/백준/Gold/2240. 자두나무/자두나무.cc
+++ C++17/백준/Gold/2240. 자두나무/자두나무.cc
@@ -1,27 +1,45 @@
 #include <iostream>
+#include <algorithm>
 #include <cstring>
 using namespace std;
 
-int t, w;
-int dp[1001][3][31];
-int b[1001];
-
-int go(int time, int tree, int n)
-{
-    if (n < 0) return -1e9;
-    if (time == t) return 0;
-    int &ret = dp[time][tree][n];
-    if (ret != -1) return ret;
-    ret = max(go(time + 1, tree, n), go(time + 1, 3 - tree, n - 1)) + (tree == b[time]);
-    return ret;
-}
-
+int dp[1001][32][2]; // 시간? 몇번 움직임? 위치?
+int T, W;
 int main(){
-    memset(dp, -1, sizeof(dp));
-    cin >> t >> w;
-    for (int i = 0; i < t; i++){
-        cin >> b[i];
+	cin >> T >> W;
+	int tmp;
+	int ans = 0;
+	memset(dp, -1, sizeof(dp)); // -1 로 초기화함.
+	dp[0][0][0] = 0; // 0초에 0 번 움직임.
+
+	for (int i = 1; i <= T; i++){ // 시간
+		cin >> tmp;
+		for (int j = 0; j <= W; j++){ // 몇번 움직임.
+			if (dp[i - 1][j][0] != -1){ // 있으면
+				if (tmp == 1){
+					dp[i][j][0] = max(dp[i][j][0], dp[i - 1][j][0] + 1);
+					dp[i][j + 1][1] = max(dp[i][j + 1][1], dp[i - 1][j][0]);
+				}
+				else { // tmp == 2 일때
+					dp[i][j + 1][1] = max(dp[i][j + 1][1], dp[i - 1][j][0] + 1);
+					dp[i][j][0] = max(dp[i][j][0], dp[i - 1][j][0]);
+				}
+			}
+			if (dp[i - 1][j][1] != -1){ // 2 나무에 있음.
+				if (tmp == 1){
+					dp[i][j + 1][0] = max(dp[i][j + 1][0], dp[i - 1][j][1] + 1);
+					dp[i][j][1] = max(dp[i][j][1], dp[i - 1][j][1]);
+				}
+				else {
+					dp[i][j + 1][0] = max(dp[i][j + 1][0], dp[i - 1][j][1]);
+					dp[i][j][1] = max(dp[i][j][1], dp[i - 1][j][1] + 1);
+				}
+			}
+		}
+	}
+	for(int j=0; j<=W; ++j) {
+        ans = max({ans, dp[T][j][0], dp[T][j][1]});
     }
-    cout << max(go(0, 1, w), go(0, 2, w - 1));
-    return 0;
+	cout << ans;
+	return 0;
 }
