fd1fd31f1847c93958d607f11f8d2470676477d4
minwucho <156387215+mauercho@users.noreply.github.com>
2026-04-27 22:17:42 +0900

    [Gold II] Title: 성냥개비, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.cc
+++ 백준/Gold/3687. 성냥개비/성냥개비.cc
@@ -1,14 +1,13 @@
 #include<bits/stdc++.h>
+//2. 탑버텀 
 using namespace std;  
-//1. 그리디와 dp(바텀업) 
 int a[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6},t, n;
-string dp[104], MAX_STR = "111111111111111111111111111111111111111111111111119";
+string dp[104], dp2[104], MAX_STR = "11111111111111111111111111111111111111111111111111111";
 string get_min_str(string a, string b){
 	if(a.size() == b.size()) return (a < b ? a : b); 
 	if(a.size() < b.size()) return a; 
-	return b;  
+	else return b;  
 }
-// 그리디 
 string findMax(int here){  
 	string ret = "";
 	if(here & 1){ret += '7'; here -= 3; }
@@ -17,23 +16,26 @@ string findMax(int here){
 		here -= 2; 
 	}
 	return ret;   
+}
+string findMin(int here){  
+	if(here == 0) return ""; 
+	string &ret = dp[here];  
+	if(ret != MAX_STR) return ret;  
+	for(int i = 0; i <= 9; i++){ 
+		if(here - a[i] < 0) continue; 
+		if(here == n && i == 0) continue; 
+		ret = get_min_str(ret, to_string(i) + findMin(here - a[i]));
+	} 
+	return ret; 
 } 
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	cin >> t;   
-	fill(dp, dp + 104, MAX_STR); 
-	dp[0] = ""; 
-	for(int i = 2; i < 104; i++){
-		for(int j = 0; j <= 9; j++){ 
-			if(i - a[j] < 0) continue;
-			if(j == 0 && dp[i - a[j]] == "")continue;
-			dp[i] = get_min_str(dp[i], dp[i - a[j]] + to_string(j));
-		}
-	}
+	cin >> t;     
 	while(t--){   
 		cin >> n;  
-		cout << dp[n] << " " << findMax(n) << "\n";
+		fill(dp, dp + 104, MAX_STR);   
+		cout << findMin(n) << " " << findMax(n) << "\n";
 	}
 }
